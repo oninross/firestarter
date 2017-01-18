708eac365c33cfab626788d2f5b0c7fff452104a
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2017-01-18 14:09:21 +1100

    - added in service worker

--- src/_assets/firestarter/js/main.js
+++ src/_assets/firestarter/js/main.js
@@ -157,3 +157,37 @@ $(() => {
 
     console.log("I'm a firestarter!");
 });
+
+
+// Simple Service Worker to make App Install work (OPTIONAL)
+window.addEventListener('load', function() {
+    var outputElement = document.getElementById('output');
+
+    navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+        .then(function(r) {
+          console.log('registered service worker');
+      })
+    .catch(function(whut) {
+        console.error('uh oh... ');
+        console.error(whut);
+    });
+
+    window.addEventListener('beforeinstallprompt', function(e) {
+        outputElement.textContent = 'beforeinstallprompt Event fired';
+    });
+});
+
+window.addEventListener('beforeinstallprompt', function(e) {
+    outputElement.textContent = 'beforeinstallprompt Event fired';
+
+    // e.userChoice will return a Promise. For more details read: http://www.html5rocks.com/en/tutorials/es6/promises/
+    e.userChoice.then(function(choiceResult) {
+        console.log(choiceResult.outcome);
+
+        if (choiceResult.outcome == 'dismissed') {
+            console.log('User cancelled homescreen install');
+        } else {
+            console.log('User added to homescreen');
+        }
+    });
+});
