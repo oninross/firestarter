ce449dda364d7052abf1f37afaf36d0401f63189
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2017-02-14 18:08:32 +1100

    - Code clean up

--- gulp/copyFonts.js
+++ gulp/copyFonts.js
@@ -6,13 +6,7 @@ export default function(gulp, plugins, args, config, taskTarget, browserSync) {
   let dirs = config.directories;
   let dest = path.join(taskTarget, dirs.fonts.replace(/^_/, ''));
 
-  // Copy
-  // gulp.task('copyFonts', () => {
-  //   return gulp.src(path.join(dirs.source, '**/*.{ttf,woff,eof,svg}'))
-  //   .pipe(plugins.changed(dest))
-  //   .pipe(gulp.dest(dest));
-  // });
-
+  // Copy fonts
   gulp.task('copyFonts', function() {
     gulp.src(path.join(dirs.source, '_assets/firestarter/css/fonts/**/*.*'))
       .pipe(gulp.dest(dest))
