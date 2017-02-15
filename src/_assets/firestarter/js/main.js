ae4978f03c42175ca0f2736628599cd495e3bdc7
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2017-02-15 14:16:00 +1100

    - removed unnecessary code

--- src/_assets/firestarter/js/main.js
+++ src/_assets/firestarter/js/main.js
@@ -161,34 +161,34 @@ $(() => {
 
 
 // Simple Service Worker to make App Install work (OPTIONAL)
-window.addEventListener('load', function () {
-    let outputElement = document.getElementById('output');
+// window.addEventListener('load', function () {
+//     let outputElement = document.getElementById('output');
 
-    navigator.serviceWorker.register('/service-worker.js', { scope: './' })
-        .then(function (r) {
-            console.log('registered service worker');
-        })
-    .catch(function (whut) {
-        console.error('uh oh... ');
-        console.error(whut);
-    });
+//     navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+//         .then(function (r) {
+//             console.log('registered service worker');
+//         })
+//     .catch(function (whut) {
+//         console.error('uh oh... ');
+//         console.error(whut);
+//     });
 
-    window.addEventListener('beforeinstallprompt', function (e) {
-        outputElement.textContent = 'beforeinstallprompt Event fired';
-    });
-});
+//     window.addEventListener('beforeinstallprompt', function (e) {
+//         outputElement.textContent = 'beforeinstallprompt Event fired';
+//     });
+// });
 
-window.addEventListener('beforeinstallprompt', function (e) {
-    outputElement.textContent = 'beforeinstallprompt Event fired';
+// window.addEventListener('beforeinstallprompt', function (e) {
+//     outputElement.textContent = 'beforeinstallprompt Event fired';
 
-    // e.userChoice will return a Promise. For more details read: http://www.html5rocks.com/en/tutorials/es6/promises/
-    e.userChoice.then(function (choiceResult) {
-        console.log(choiceResult.outcome);
+//     // e.userChoice will return a Promise. For more details read: http://www.html5rocks.com/en/tutorials/es6/promises/
+//     e.userChoice.then(function (choiceResult) {
+//         console.log(choiceResult.outcome);
 
-        if (choiceResult.outcome == 'dismissed') {
-            console.log('User cancelled homescreen install');
-        } else {
-            console.log('User added to homescreen');
-        }
-    });
-});
+//         if (choiceResult.outcome == 'dismissed') {
+//             console.log('User cancelled homescreen install');
+//         } else {
+//             console.log('User added to homescreen');
+//         }
+//     });
+// });
