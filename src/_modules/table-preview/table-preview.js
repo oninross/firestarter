3cbe220eb9fa17ad13452376e34c442567c6a653
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2017-02-17 17:38:26 +1100

    - manifest cleanup - made mobile table wrapper fancier

--- src/_modules/table-preview/table-preview.js
+++ src/_modules/table-preview/table-preview.js
@@ -9,13 +9,53 @@ export default class TablePreview {
         $('table').each(function (i, v) {
             let $this = $(v);
 
-            if ($this.length && !$this.parent().hasClass('table-wrapper') && $this.isTableWide()) {
+            if ($this.length && $this.isTableWide()) {
                 $this
                     .after('<button class="btn-print-table js-print-table">View Table</button>')
-                    .wrap('<div class="table-wrapper"></div>');
+                    .wrap('<div class="table-wrap"><div class="table-responsive"></div></div>');
             }
         });
 
+        $('.table-wrap').each(function () {
+            var $this = $(this),
+                $tableResponsive = $this.find('.table-responsive'),
+                $table = $this.find('table'),
+                trTotalWidth = getTrWidth($this, $table);
+
+            isLeftOrRight($this, $tableResponsive, trTotalWidth);
+
+            $tableResponsive.on('scroll', function () {
+                isLeftOrRight($this, $tableResponsive, trTotalWidth);
+            })
+        });
+
+        function getTrWidth(el, table) {
+            var width,
+                totalWidth = 0,
+                tableWrapWidth = el.width();
+
+            return table.find('tr:first-child > *').each(function () {
+                totalWidth += $(this).outerWidth()
+            }), width = totalWidth - tableWrapWidth;
+        };
+
+        function isLeftOrRight(el, tableResponsive, trTotalWidth) {
+            var tableResponsiveScrollLeft = tableResponsive.scrollLeft();
+            tableResponsiveScrollLeft > 0 ? el.addClass('left') : el.removeClass('left'),
+            tableResponsiveScrollLeft < trTotalWidth ? el.addClass('right') : el.removeClass('right')
+        };
+
+
+
+        // Click the button to open table in new tab
+        $('body').on('click', '.js-print-table', function () {
+            let $table = $(this).prev();
+
+            localStorage.tablePreview = $table[0].innerHTML;
+            window.open('/table-preview/', '_blank').focus();
+        });
+
+        // Display the table in new tab
         let $tablePreview = $('.table-preview');
         if ($tablePreview.length) {
             $('meta[name="viewport"]').attr('content', 'user-scalable=yes');
@@ -25,12 +65,5 @@ export default class TablePreview {
                 localStorage.tablePreview = null;
             });
         }
-
-        $('body').on('click', '.js-print-table', function () {
-            let $table = $(this).prev();
-
-            localStorage.tablePreview = $table[0].innerHTML;
-            window.open('/table-preview/', '_blank').focus();
-        });
     }
 }
