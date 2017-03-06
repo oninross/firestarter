9fd18250d5d84fe73246b76d3c7e39de7e337236
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2017-03-06 15:24:31 +1100

    - version number bump - updated service worker to Google's version - removed fonts.  using system fonts according to Medium - fixed manifest for assets

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,208 +1,152 @@
-"use strict";
-
-console.log('WORKER: executing.');
-
-/* A version number is useful when updating the worker logic,
-   allowing you to remove outdated cache entries during the update.
+/*
+ Copyright 2014 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 */
-var version = '1.2.0';
 
-/* These resources will be downloaded and cached by the service worker
-   during the installation process. If any resource fails to be downloaded,
-   then the service worker won't be installed either.
-*/
-var offlineFundamentals = [
-    'assets/firestarter/css/main.css',
-    'assets/firestarter/js/main.js',
-    'assets/firestarter/css/fonts/icomoon.woff',
-    'index.html',
-    '/'
-];
+// This polyfill provides Cache.add(), Cache.addAll(), and CacheStorage.match(),
+// which are not implemented in Chrome 40.
+importScripts('serviceworker-cache-polyfill.js');
 
-/* The install event fires when the service worker is first installed.
-   You can use this event to prepare the service worker to be able to serve
-   files while visitors are offline.
-*/
-self.addEventListener("install", function (event) {
-    console.log('WORKER: install event in progress.');
-    /* Using event.waitUntil(p) blocks the installation process on the provided
-       promise. If the promise is rejected, the service worker won't be installed.
-    */
+// While overkill for this specific sample in which there is only one cache,
+// this is one best practice that can be followed in general to keep track of
+// multiple caches used by a given service worker, and keep them all versioned.
+// It maps a shorthand identifier for a cache to a specific, versioned cache name.
+
+// Note that since global state is discarded in between service worker restarts, these
+// variables will be reinitialized each time the service worker handles an event, and you
+// should not attempt to change their values inside an event handler. (Treat them as constants.)
+
+// If at any point you want to force pages that use this service worker to start using a fresh
+// cache, then increment the CACHE_VERSION value. It will kick off the service worker update
+// flow and the old cache(s) will be purged as part of the activate event handler when the
+// updated service worker is activated.
+var version = '1.2.1',
+    CURRENT_CACHES = {
+        prefetch: 'prefetch-cache-v' + version
+    };
+
+self.addEventListener('install', function (event) {
+    var now = Date.now();
+
+    var urlsToPrefetch = [
+        'assets/firestarter/css/main.css',
+        'assets/firestarter/js/main.js',
+        'assets/firestarter/css/fonts/icomoon.woff',
+        'index.html',
+        '/'
+    ];
+
+    // All of these logging statements should be visible via the "Inspect" interface
+    // for the relevant SW accessed via chrome://serviceworker-internals
+    console.log('Handling install event. Resources to prefetch:', urlsToPrefetch);
+
     event.waitUntil(
-        /* The caches built-in is a promise-based API that helps you cache responses,
-           as well as finding and deleting them.
-        */
-        caches
-        /* You can open a cache by name, and this method returns a promise. We use
-           a versioned cache name here so that we can remove old cache entries in
-           one fell swoop later, when phasing out an older service worker.
-        */
-        .open(version + 'fundamentals')
-        .then(function (cache) {
-            /* After the cache is opened, we can fill it with the offline fundamentals.
-               The method below will add all resources in `offlineFundamentals` to the
-               cache, after making requests for them.
-            */
-            return cache.addAll(offlineFundamentals);
-        })
-        .then(function () {
-            console.log('WORKER: install completed');
-        })
-    );
-});
+        caches.open(CURRENT_CACHES.prefetch).then(function (cache) {
+            var cachePromises = urlsToPrefetch.map(function (urlToPrefetch) {
+                // This constructs a new URL object using the service worker's script location as the base
+                // for relative URLs.
+                var url = new URL(urlToPrefetch, location.href);
+                // Append a cache-bust=TIMESTAMP URL parameter to each URL's query string.
+                // This is particularly important when precaching resources that are later used in the
+                // fetch handler as responses directly, without consulting the network (i.e. cache-first).
+                // If we were to get back a response from the HTTP browser cache for this precaching request
+                // then that stale response would be used indefinitely, or at least until the next time
+                // the service worker script changes triggering the install flow.
+                url.search += (url.search ? '&' : '?') + 'cache-bust=' + now;
 
-/* The fetch event fires whenever a page controlled by this service worker requests
-   a resource. This isn't limited to `fetch` or even XMLHttpRequest. Instead, it
-   comprehends even the request for the HTML page on first load, as well as JS and
-   CSS resources, fonts, any images, etc.
-*/
-self.addEventListener("fetch", function (event) {
-    console.log('WORKER: fetch event in progress.');
+                // It's very important to use {mode: 'no-cors'} if there is any chance that
+                // the resources being fetched are served off of a server that doesn't support
+                // CORS (http://en.wikipedia.org/wiki/Cross-origin_resource_sharing).
+                // In this example, www.chromium.org doesn't support CORS, and the fetch()
+                // would fail if the default mode of 'cors' was used for the fetch() request.
+                // The drawback of hardcoding {mode: 'no-cors'} is that the response from all
+                // cross-origin hosts will always be opaque
+                // (https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#cross-origin-resources)
+                // and it is not possible to determine whether an opaque response represents a success or failure
+                // (https://github.com/whatwg/fetch/issues/14).
+                var request = new Request(url, {mode: 'no-cors'});
+                return fetch(request).then(function (response) {
+                    if (response.status >= 400) {
+                        throw new Error('request for ' + urlToPrefetch +
+                            ' failed with status ' + response.statusText);
+                    }
 
-    /* We should only cache GET requests, and deal with the rest of method in the
-       client-side, by handling failed POST,PUT,PATCH,etc. requests.
-    */
-    if (event.request.method !== 'GET') {
-        /* If we don't block the event as shown below, then the request will go to
-           the network as usual.
-        */
-        console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
-        return;
-    }
-    /* Similar to event.waitUntil in that it blocks the fetch event on a promise.
-       Fulfillment result will be used as the response, and rejection will end in a
-       HTTP response indicating failure.
-    */
-    event.respondWith(
-        caches
-        /* This method returns a promise that resolves to a cache entry matching
-           the request. Once the promise is settled, we can then provide a response
-           to the fetch request.
-        */
-        .match(event.request)
-        .then(function (cached) {
-            /* Even if the response is in our cache, we go to the network as well.
-               This pattern is known for producing "eventually fresh" responses,
-               where we return cached responses immediately, and meanwhile pull
-               a network response and store that in the cache.
-
-               Read more:
-               https://ponyfoo.com/articles/progressive-networking-serviceworker
-            */
-            var networked = fetch(event.request)
-                // We handle the network request with success and failure scenarios.
-                .then(fetchedFromNetwork, unableToResolve)
-                // We should catch errors on the fetchedFromNetwork handler as well.
-                .catch(unableToResolve);
-
-            /* We return the cached response immediately if there is one, and fall
-               back to waiting on the network as usual.
-            */
-            console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
-            return cached || networked;
-
-            function fetchedFromNetwork(response) {
-                /* We copy the response before replying to the network request.
-                   This is the response that will be stored on the ServiceWorker cache.
-                */
-                var cacheCopy = response.clone();
-
-                console.log('WORKER: fetch response from network.', event.request.url);
-
-                caches
-                // We open a cache to store the response for this request.
-                    .open(version + 'pages')
-                    .then(function add(cache) {
-                        /* We store the response for this request. It'll later become
-                           available to caches.match(event.request) calls, when looking
-                           for cached responses.
-                        */
-                        cache.put(event.request, cacheCopy);
-                    })
-                    .then(function () {
-                        console.log('WORKER: fetch response stored in cache.', event.request.url);
-                    });
-
-                // Return the response so that the promise is settled in fulfillment.
-                return response;
-            }
-
-            /* When this method is called, it means we were unable to produce a response
-               from either the cache or the network. This is our opportunity to produce
-               a meaningful response even when all else fails. It's the last chance, so
-               you probably want to display a "Service Unavailable" view or a generic
-               error response.
-            */
-            function unableToResolve() {
-                /* There's a couple of things we can do here.
-                   - Test the Accept header and then return one of the `offlineFundamentals`
-                     e.g: `return caches.match('/some/cached/image.png')`
-                   - You should also consider the origin. It's easier to decide what
-                     "unavailable" means for requests against your origins than for requests
-                     against a third party, such as an ad provider.
-                   - Generate a Response programmaticaly, as shown below, and return that.
-                */
-
-                console.log('WORKER: fetch request failed in both cache and network.');
-
-                /* Here we're creating a response programmatically. The first parameter is the
-                   response body, and the second one defines the options for the response.
-                */
-                return new Response('<h1>Service Unavailable</h1>', {
-                    status: 503,
-                    statusText: 'Service Unavailable',
-                    headers: new Headers({
-                        'Content-Type': 'text/html'
-                    })
+                    // Use the original URL without the cache-busting parameter as the key for cache.put().
+                    return cache.put(urlToPrefetch, response);
+                }).catch(function (error) {
+                    console.error('Not caching ' + urlToPrefetch + ' due to ' + error);
                 });
-            }
+            });
+
+            return Promise.all(cachePromises).then(function () {
+                console.log('Pre-fetching complete.');
+            });
+        }).catch(function (error) {
+            console.error('Pre-fetching failed:', error);
         })
     );
 });
 
-/* The activate event fires after a service worker has been successfully installed.
-   It is most useful when phasing out an older version of a service worker, as at
-   this point you know that the new worker was installed correctly. In this example,
-   we delete old caches that don't match the version in the worker we just finished
-   installing.
-*/
-self.addEventListener("activate", function (event) {
-    /* Just like with the install event, event.waitUntil blocks activate on a promise.
-       Activation will fail unless the promise is fulfilled.
-    */
-    console.log('WORKER: activate event in progress.');
+self.addEventListener('activate', function (event) {
+    // Delete all caches that aren't named in CURRENT_CACHES.
+    // While there is only one cache in this example, the same logic will handle the case where
+    // there are multiple versioned caches.
+    var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
+        return CURRENT_CACHES[key];
+    });
 
     event.waitUntil(
-        caches
-        /* This method returns a promise which will resolve to an array of available
-           cache keys.
-        */
-        .keys()
-        .then(function (keys) {
-            // We return a promise that settles when all outdated caches are deleted.
+        caches.keys().then(function (cacheNames) {
             return Promise.all(
-                keys
-                .filter(function (key) {
-                    // Filter by keys that don't start with the latest version prefix.
-                    return !key.startsWith(version);
-                })
-                .map(function (key) {
-                    /* Return a promise that's fulfilled
-                       when each outdated cache is deleted.
-                    */
-                    return caches.delete(key);
+                cacheNames.map(function (cacheName) {
+                    if (expectedCacheNames.indexOf(cacheName) === -1) {
+                        // If this cache name isn't present in the array of "expected" cache names, then delete it.
+                        console.log('Deleting out of date cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
                 })
             );
         })
-        .then(function () {
-            console.log('WORKER: activate completed.');
-        })
     );
 });
 
-/* Push Notifications
-*/
-self.addEventListener("push", function (event) {
-    console.log('Push message received', event);
-    // TODO
+self.addEventListener('fetch', function (event) {
+    console.log('Handling fetch event for', event.request.url);
+
+    event.respondWith(
+        // caches.match() will look for a cache entry in all of the caches available to the service worker.
+        // It's an alternative to first opening a specific named cache and then matching on that.
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                console.log('Found response in cache:', response);
+
+                return response;
+            }
+
+            console.log('No response found in cache. About to fetch from network...');
+
+            // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
+            // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
+            return fetch(event.request).then(function (response) {
+                console.log('Response from network is:', response);
+
+                return response;
+            }).catch(function (error) {
+                // This catch() will handle exceptions thrown from the fetch() operation.
+                // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
+                // It will return a normal response object that has the appropriate error code set.
+                console.error('Fetching failed:', error);
+
+                throw error;
+            });
+        })
+    );
 });
