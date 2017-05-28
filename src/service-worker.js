56cde32f95b1c4cdca8c2fd23cd182c22e0b0a2d
Nino Ross Rodriguez <oninross@gmail.com>
2017-05-28 13:43:42 +1000

    - commented out service workers for easy development - fix indentation of html tag - version number bump

--- src/service-worker.js
+++ src/service-worker.js
@@ -24,7 +24,7 @@
 // cache, then increment the CACHE_VERSION value. It will kick off the service worker update
 // flow and the old cache(s) will be purged as part of the activate event handler when the
 // updated service worker is activated.
-var version = '1.3.0',
+var version = '1.3.3',
     now = Date.now(),
     OFFLINE_URL = 'offline/',
     urlsToPrefetch = [
