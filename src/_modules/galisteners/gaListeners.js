2e32e8f32472a6dd71762d8894b17c0594d61dd7
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2018-08-29 16:56:21 +1000

    - house cleaning

--- src/_modules/galisteners/gaListeners.js
+++ src/_modules/galisteners/gaListeners.js
@@ -4,11 +4,11 @@ export default class Galisteners {
     constructor() { }
 
     init() {
-        (function (i,s,o,g,r,a,m) {
+        (function (i, s, o, g, r, a, m) {
             i.GoogleAnalyticsObject = r; i[r] = i[r] || function () {
                 (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
+            }, i[r].l = 1 * new Date(); a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
             a.async = 1;
             a.src = g;
             m.parentNode.insertBefore(a, m)
