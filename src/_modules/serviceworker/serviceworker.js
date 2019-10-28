0fa46858462f7d37b1033dc1822ebf4b4a045cb8
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2019-10-28 14:18:57 +1100

    - JS fix to get that A2HS banner

--- src/_modules/serviceworker/serviceworker.js
+++ src/_modules/serviceworker/serviceworker.js
@@ -44,6 +44,9 @@ export default class ServiceWorker {
       });
 
       window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent Chrome 76 and later from showing the mini-infobar
+        e.preventDefault();
+
         // Stash the event so it can be triggered later.
         deferredPrompt = e;
 
@@ -78,5 +81,9 @@ export default class ServiceWorker {
         };
       }
     }
+  }
+
+  showInstallPromotion() {
+    const self = this;
   }
 }
