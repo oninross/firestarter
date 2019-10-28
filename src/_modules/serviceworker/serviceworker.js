f36fff6d68324e8abc503a5995a59e4ffc915f6f
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2019-10-28 12:38:43 +1100

    - JS fix on Service Workers

--- src/_modules/serviceworker/serviceworker.js
+++ src/_modules/serviceworker/serviceworker.js
@@ -8,11 +8,6 @@ export default class ServiceWorker {
 
     const material = new MaterialDesign();
 
-    /*
-    * https://developers.google.com/web/fundamentals/app-install-banners/
-    * Look out for this link when Chrome 68 rolls out
-    */
-
     if ('serviceWorker' in navigator) {
       let deferredPrompt;
 
@@ -45,27 +40,26 @@ export default class ServiceWorker {
               console.error('uh oh... ');
               console.error(whut);
             });
-
-          // Show the prompt
-          deferredPrompt.prompt();
-          // Wait for the user to respond to the prompt
-          deferredPrompt.userChoice
-            .then((choiceResult) => {
-              if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-              } else {
-                console.log('User dismissed the A2HS prompt');
-              }
-              deferredPrompt = null;
-            });
         }
       });
 
       window.addEventListener('beforeinstallprompt', (e) => {
-        // Prevent Chrome 67 and earlier from automatically showing the prompt
-        e.preventDefault();
         // Stash the event so it can be triggered later.
         deferredPrompt = e;
+
+        // Show the prompt
+        deferredPrompt.prompt();
+
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice
+          .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+              console.log('User accepted the A2HS prompt');
+            } else {
+              console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+          });
       });
 
       window.addEventListener('appinstalled', (evt) => {
