58c5dd196d04d1d24a54987fa0def15c8a68c83b
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2019-10-28 13:47:55 +1100

    - added docs folder

---
+++ docs/service-worker.js
@@ -0,0 +1,178 @@
+/*
+ Copyright 2014 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+// While overkill for this specific sample in which there is only one cache,
+// this is one best practice that can be followed in general to keep track of
+// multiple caches used by a given service worker, and keep them all versioned.
+// It maps a shorthand identifier for a cache to a specific, versioned cache name.
+
+// Note that since global state is discarded in between service worker restarts, these
+// variables will be reinitialized each time the service worker handles an event, and you
+// should not attempt to change their values inside an event handler. (Treat them as constants.)
+
+// If at any point you want to force pages that use this service worker to start using a fresh
+// cache, then increment the CACHE_VERSION value. It will kick off the service worker update
+// flow and the old cache(s) will be purged as part of the activate event handler when the
+// updated service worker is activated.
+var version = '1.8.1';
+var now = Date.now();
+var OFFLINE_URL = '/pages/offline/';
+var CURRENT_CACHES = {
+    prefetch: 'prefetch-cache-v' + version
+};
+var urlsToPrefetch = [
+    'assets/firestarter/css/main.css',
+    'assets/firestarter/js/main.js',
+    'assets/firestarter/fonts/icomoon.woff',
+    'manifest.json',
+    OFFLINE_URL,
+    'index.html?homescreen=1',
+    '/'
+];
+
+self.addEventListener('install', function (event) {
+    // All of these logging statements should be visible via the "Inspect" interface
+    // for the relevant SW accessed via chrome://serviceworker-internals
+    console.log('Handling install event. Resources to prefetch:', urlsToPrefetch);
+
+    event.waitUntil(
+        caches.open(CURRENT_CACHES.prefetch).then(function (cache) {
+            var cachePromises = urlsToPrefetch.map(function (urlToPrefetch) {
+                // This constructs a new URL object using the service worker's script location as the base
+                // for relative URLs.
+                var url = new URL(urlToPrefetch, location.href);
+                // Append a cache-bust=TIMESTAMP URL parameter to each URL's query string.
+                // This is particularly important when precaching resources that are later used in the
+                // fetch handler as responses directly, without consulting the network (i.e. cache-first).
+                // If we were to get back a response from the HTTP browser cache for this precaching request
+                // then that stale response would be used indefinitely, or at least until the next time
+                // the service worker script changes triggering the install flow.
+                url.search += (url.search ? '&' : '?') + 'cache-bust=' + now;
+
+                // It's very important to use {mode: 'no-cors'} if there is any chance that
+                // the resources being fetched are served off of a server that doesn't support
+                // CORS (http://en.wikipedia.org/wiki/Cross-origin_resource_sharing).
+                // In this example, www.chromium.org doesn't support CORS, and the fetch()
+                // would fail if the default mode of 'cors' was used for the fetch() request.
+                // The drawback of hardcoding {mode: 'no-cors'} is that the response from all
+                // cross-origin hosts will always be opaque
+                // (https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#cross-origin-resources)
+                // and it is not possible to determine whether an opaque response represents a success or failure
+                // (https://github.com/whatwg/fetch/issues/14).
+                var request = new Request(url, { mode: 'no-cors' });
+                return fetch(request).then(function (response) {
+                    if (response.status >= 400) {
+                        throw new Error('request for ' + urlToPrefetch +
+                            ' failed with status ' + response.statusText);
+                    }
+
+                    // Use the original URL without the cache-busting parameter as the key for cache.put().
+                    return cache.put(urlToPrefetch, response);
+                }).catch(function (error) {
+                    console.error('Not caching ' + urlToPrefetch + ' due to ' + error);
+                });
+            });
+
+            return Promise.all(cachePromises).then(function () {
+                console.log('Pre-fetching complete.');
+            });
+        }).then(function () {
+            // `skipWaiting()` forces the waiting ServiceWorker to become the
+            // active ServiceWorker, triggering the `onactivate` event.
+            // Together with `Clients.claim()` this allows a worker to take effect
+            // immediately in the client(s).
+            return self.skipWaiting();
+        }).catch(function (error) {
+            console.error('Pre-fetching failed:', error);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    // Delete all caches that aren't named in CURRENT_CACHES.
+    // While there is only one cache in this example, the same logic will handle the case where
+    // there are multiple versioned caches.
+    var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
+        return CURRENT_CACHES[key];
+    });
+
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (expectedCacheNames.indexOf(cacheName) === -1) {
+                        // If this cache name isn't present in the array of "expected" cache names, then delete it.
+                        console.log('Deleting out of date cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('Handling fetch event for', event.request.url);
+
+    event.respondWith(
+        caches.open(CURRENT_CACHES.prefetch).then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                if (response) {
+                    // If there is an entry in the cache for event.request, then response will be defined
+                    // and we can just return it.
+                    console.log(' Found response in cache:', response);
+
+                    return response;
+                }
+
+                // Otherwise, if there is no entry in the cache for event.request, response will be
+                // undefined, and we need to fetch() the resource.
+                console.log(' No response for %s found in cache. ' +
+                    'About to fetch from network...', event.request.url);
+
+                // We call .clone() on the request since we might use it in the call to cache.put() later on.
+                // Both fetch() and cache.put() "consume" the request, so we need to make a copy.
+                // (see https://fetch.spec.whatwg.org/#dom-request-clone)
+                return fetch(event.request.clone()).then(function (response) {
+                    console.log('  Response for %s from network is: %O',
+                        event.request.url, response);
+
+                    // Optional: add in extra conditions here, e.g. response.type == 'basic' to only cache
+                    // responses from the same domain. See https://fetch.spec.whatwg.org/#concept-response-type
+                    if (response.status < 400) {
+                        // This avoids caching responses that we know are errors (i.e. HTTP status code of 4xx or 5xx).
+                        // One limitation is that, for non-CORS requests, we get back a filtered opaque response
+                        // (https://fetch.spec.whatwg.org/#concept-filtered-response-opaque) which will always have a
+                        // .status of 0, regardless of whether the underlying HTTP call was successful. Since we're
+                        // blindly caching those opaque responses, we run the risk of caching a transient error response.
+                        //
+                        // We need to call .clone() on the response object to save a copy of it to the cache.
+                        // (https://fetch.spec.whatwg.org/#dom-request-clone)
+                        cache.put(event.request, response.clone());
+                    }
+
+                    // Return the original response object, which will be used to fulfill the resource request.
+                    return response;
+                });
+            }).catch(function (error) {
+                // This catch() will handle exceptions that arise from the match() or fetch() operations.
+                // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
+                // It will return a normal response object that has the appropriate error code set.
+                console.error('  Read-through caching failed:', error);
+
+                // throw error;
+                return caches.match(OFFLINE_URL);
+            });
+        })
+    );
+});
